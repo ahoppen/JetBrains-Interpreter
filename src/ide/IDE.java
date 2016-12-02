c77487d13c7d729fd462062de4565251d2142b21
Alex Hoppen <alex@ateamer.de>
2016-12-02 16:05:14 +0000

    Display “pi = “ and the value in the same line in the sample code

--- src/ide/IDE.java
+++ src/ide/IDE.java
@@ -48,8 +48,7 @@ public class IDE extends Application {
             "var n = 1000",
             "var sequence = map({0, n}, i -> (-1)^i / (2 * i + 1))",
             "var pi = 4 * reduce(sequence, 0, x y -> x + y)",
-            "print \"pi = \"",
-            "out pi"
+            "print \"pi = \" out pi"
     });
 
     private Stage stage;
