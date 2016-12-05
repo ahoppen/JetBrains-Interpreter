e83f784cc6b46e56a2a83f1d63ae3d056b28ea27
Alex Hoppen <alex@ateamer.de>
2016-12-05 10:29:12 +0000

    Add test cases for variables that are used in their definition

--- test/typeChecker/variableScope.ml
+++ test/typeChecker/variableScope.ml
@@ -4,3 +4,7 @@ var a = 2
 var c = map({1, 5}, x -> x * a) # expectedError@30: Variable 'a' referenced before declaration
 
 var d = map({1, 5}, a -> a)
+
+var x = reduce({0, x}, 0, a b -> a + b) # expectedError@20: Variable 'x' referenced before declaration
+
+var y = y # expectedError@9: Variable 'y' referenced before declaration
